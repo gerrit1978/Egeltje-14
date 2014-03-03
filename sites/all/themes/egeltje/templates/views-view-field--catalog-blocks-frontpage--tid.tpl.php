b891fdbaa7fa96dbb19b1412ea82c35aad340160
Gerrit Vos <gevo27@gmail.com>
2014-03-03 17:58:17 +0100

    slicing

---
+++ sites/all/themes/egeltje/templates/views-view-field--catalog-blocks-frontpage--tid.tpl.php
@@ -0,0 +1,12 @@
+<?php
+$term = $row->_field_data['tid']['entity'];
+
+$category = $term->name;
+$category_url = url('product_category/' . $term->tid);
+$color_dark_field = field_get_items('taxonomy_term', $term, 'field_catalog_color_dark');
+$color = $color_dark_field[0]['rgb'];
+
+?>
+<a href="<?php print $category_url; ?>" style="background-color:<?php print $color; ?>;">
+<div class='category'><?php print $category; ?></div>
+</a>
