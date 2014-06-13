c659c125a839bb7e59cc404305dacda263ca9963
Gerrit Vos <gevo27@gmail.com>
2014-06-13 08:33:33 +0200

    Commerce Kickstart update

---
+++ sites/default/files/js/js_Bf4zRnqxHRbosgTugD2snunyU_wimxqzSSHnuIYWzcY.js
@@ -0,0 +1,507 @@
+(function ($) {
+
+/**
+ * A progressbar object. Initialized with the given id. Must be inserted into
+ * the DOM afterwards through progressBar.element.
+ *
+ * method is the function which will perform the HTTP request to get the
+ * progress bar state. Either "GET" or "POST".
+ *
+ * e.g. pb = new progressBar('myProgressBar');
+ *      some_element.appendChild(pb.element);
+ */
+Drupal.progressBar = function (id, updateCallback, method, errorCallback) {
+  var pb = this;
+  this.id = id;
+  this.method = method || 'GET';
+  this.updateCallback = updateCallback;
+  this.errorCallback = errorCallback;
+
+  // The WAI-ARIA setting aria-live="polite" will announce changes after users
+  // have completed their current activity and not interrupt the screen reader.
+  this.element = $('<div class="progress" aria-live="polite"></div>').attr('id', id);
+  this.element.html('<div class="bar"><div class="filled"></div></div>' +
+                    '<div class="percentage"></div>' +
+                    '<div class="message">&nbsp;</div>');
+};
+
+/**
+ * Set the percentage and status message for the progressbar.
+ */
+Drupal.progressBar.prototype.setProgress = function (percentage, message) {
+  if (percentage >= 0 && percentage <= 100) {
+    $('div.filled', this.element).css('width', percentage + '%');
+    $('div.percentage', this.element).html(percentage + '%');
+  }
+  $('div.message', this.element).html(message);
+  if (this.updateCallback) {
+    this.updateCallback(percentage, message, this);
+  }
+};
+
+/**
+ * Start monitoring progress via Ajax.
+ */
+Drupal.progressBar.prototype.startMonitoring = function (uri, delay) {
+  this.delay = delay;
+  this.uri = uri;
+  this.sendPing();
+};
+
+/**
+ * Stop monitoring progress via Ajax.
+ */
+Drupal.progressBar.prototype.stopMonitoring = function () {
+  clearTimeout(this.timer);
+  // This allows monitoring to be stopped from within the callback.
+  this.uri = null;
+};
+
+/**
+ * Request progress data from server.
+ */
+Drupal.progressBar.prototype.sendPing = function () {
+  if (this.timer) {
+    clearTimeout(this.timer);
+  }
+  if (this.uri) {
+    var pb = this;
+    // When doing a post request, you need non-null data. Otherwise a
+    // HTTP 411 or HTTP 406 (with Apache mod_security) error may result.
+    $.ajax({
+      type: this.method,
+      url: this.uri,
+      data: '',
+      dataType: 'json',
+      success: function (progress) {
+        // Display errors.
+        if (progress.status == 0) {
+          pb.displayError(progress.data);
+          return;
+        }
+        // Update display.
+        pb.setProgress(progress.percentage, progress.message);
+        // Schedule next timer.
+        pb.timer = setTimeout(function () { pb.sendPing(); }, pb.delay);
+      },
+      error: function (xmlhttp) {
+        pb.displayError(Drupal.ajaxError(xmlhttp, pb.uri));
+      }
+    });
+  }
+};
+
+/**
+ * Display errors on the page.
+ */
+Drupal.progressBar.prototype.displayError = function (string) {
+  var error = $('<div class="messages error"></div>').html(string);
+  $(this.element).before(error).hide();
+
+  if (this.errorCallback) {
+    this.errorCallback(this);
+  }
+};
+
+})(jQuery);
+;
+/**
+ * @file select_or_other.js
+ */
+
+(function ($) {
+
+  function select_or_other_check_and_show(ele, page_init) {
+    var speed;
+    if (page_init) {
+      speed = 0;
+    }
+    else {
+      speed = 200;
+      ele = jQuery(ele).parents(".select-or-other")[0];
+    }
+    if (jQuery(ele).find(".select-or-other-select option:selected[value=select_or_other], .select-or-other-select:checked[value=select_or_other]").length) {
+      jQuery(ele).find(".select-or-other-other").parents("div.form-item").first().show(speed, function() {
+        if(!page_init) {
+          $(this).find(".select-or-other-other").focus();
+        }
+      });
+    }
+    else {
+      jQuery(ele).find(".select-or-other-other").parents("div.form-item").first().hide(speed);      
+      if (page_init)
+      {
+        // Special case, when the page is loaded, also apply 'display: none' in case it is
+        // nested inside an element also hidden by jquery - such as a collapsed fieldset.
+        jQuery(ele).find(".select-or-other-other").parents("div.form-item").first().css("display", "none");
+      }
+    }
+  }
+
+  /**
+   * The Drupal behaviors for the Select (or other) field.
+   */
+  Drupal.behaviors.select_or_other = {
+    attach: function(context) {
+      jQuery(".select-or-other:not('.select-or-other-processed')", context)
+        .addClass('select-or-other-processed')
+        .each(function () {
+          select_or_other_check_and_show(this, true);
+        });
+      jQuery(".select-or-other-select", context)
+        .not("select")
+        .click(function () {
+          select_or_other_check_and_show(this, false);
+        });
+      jQuery("select.select-or-other-select", context)
+        .change(function () {
+          select_or_other_check_and_show(this, false);
+        });
+    }
+  };
+
+})(jQuery);
+;
+(function ($) {
+
+Drupal.behaviors.textarea = {
+  attach: function (context, settings) {
+    $('.form-textarea-wrapper.resizable', context).once('textarea', function () {
+      var staticOffset = null;
+      var textarea = $(this).addClass('resizable-textarea').find('textarea');
+      var grippie = $('<div class="grippie"></div>').mousedown(startDrag);
+
+      grippie.insertAfter(textarea);
+
+      function startDrag(e) {
+        staticOffset = textarea.height() - e.pageY;
+        textarea.css('opacity', 0.25);
+        $(document).mousemove(performDrag).mouseup(endDrag);
+        return false;
+      }
+
+      function performDrag(e) {
+        textarea.height(Math.max(32, staticOffset + e.pageY) + 'px');
+        return false;
+      }
+
+      function endDrag(e) {
+        $(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
+        textarea.css('opacity', 1);
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+
+(function($) {
+
+/**
+ * Drupal FieldGroup object.
+ */
+Drupal.FieldGroup = Drupal.FieldGroup || {};
+Drupal.FieldGroup.Effects = Drupal.FieldGroup.Effects || {};
+Drupal.FieldGroup.groupWithfocus = null;
+
+Drupal.FieldGroup.setGroupWithfocus = function(element) {
+  element.css({display: 'block'});
+  Drupal.FieldGroup.groupWithfocus = element;
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processFieldset = {
+  execute: function (context, settings, type) {
+    if (type == 'form') {
+      // Add required fields mark to any fieldsets containing required fields
+      $('fieldset.fieldset', context).once('fieldgroup-effects', function(i) {
+        if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
+          $('legend span.fieldset-legend', $(this)).eq(0).append(' ').append($('.form-required').eq(0).clone());
+        }
+        if ($('.error', $(this)).length) {
+          $('legend span.fieldset-legend', $(this)).eq(0).addClass('error');
+          Drupal.FieldGroup.setGroupWithfocus($(this));
+        }
+      });
+    }
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processAccordion = {
+  execute: function (context, settings, type) {
+    $('div.field-group-accordion-wrapper', context).once('fieldgroup-effects', function () {
+      var wrapper = $(this);
+
+      wrapper.accordion({
+        autoHeight: false,
+        active: '.field-group-accordion-active',
+        collapsible: true,
+        changestart: function(event, ui) {
+          if ($(this).hasClass('effect-none')) {
+            ui.options.animated = false;
+          }
+          else {
+            ui.options.animated = 'slide';
+          }
+        }
+      });
+
+      if (type == 'form') {
+
+        var $firstErrorItem = false;
+
+        // Add required fields mark to any element containing required fields
+        wrapper.find('div.field-group-accordion-item').each(function(i) {
+
+          if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
+            $('h3.ui-accordion-header a').eq(i).append(' ').append($('.form-required').eq(0).clone());
+          }
+          if ($('.error', $(this)).length) {
+            // Save first error item, for focussing it.
+            if (!$firstErrorItem) {
+              $firstErrorItem = $(this).parent().accordion("activate" , i);
+            }
+            $('h3.ui-accordion-header').eq(i).addClass('error');
+          }
+        });
+
+        // Save first error item, for focussing it.
+        if (!$firstErrorItem) {
+          $('.ui-accordion-content-active', $firstErrorItem).css({height: 'auto', width: 'auto', display: 'block'});
+        }
+
+      }
+    });
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processHtabs = {
+  execute: function (context, settings, type) {
+    if (type == 'form') {
+      // Add required fields mark to any element containing required fields
+      $('fieldset.horizontal-tabs-pane', context).once('fieldgroup-effects', function(i) {
+        if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
+          $(this).data('horizontalTab').link.find('strong:first').after($('.form-required').eq(0).clone()).after(' ');
+        }
+        if ($('.error', $(this)).length) {
+          $(this).data('horizontalTab').link.parent().addClass('error');
+          Drupal.FieldGroup.setGroupWithfocus($(this));
+          $(this).data('horizontalTab').focus();
+        }
+      });
+    }
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ */
+Drupal.FieldGroup.Effects.processTabs = {
+  execute: function (context, settings, type) {
+    if (type == 'form') {
+      // Add required fields mark to any fieldsets containing required fields
+      $('fieldset.vertical-tabs-pane', context).once('fieldgroup-effects', function(i) {
+        if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
+          $(this).data('verticalTab').link.find('strong:first').after($('.form-required').eq(0).clone()).after(' ');
+        }
+        if ($('.error', $(this)).length) {
+          $(this).data('verticalTab').link.parent().addClass('error');
+          Drupal.FieldGroup.setGroupWithfocus($(this));
+          $(this).data('verticalTab').focus();
+        }
+      });
+    }
+  }
+}
+
+/**
+ * Implements Drupal.FieldGroup.processHook().
+ *
+ * TODO clean this up meaning check if this is really
+ *      necessary.
+ */
+Drupal.FieldGroup.Effects.processDiv = {
+  execute: function (context, settings, type) {
+
+    $('div.collapsible', context).once('fieldgroup-effects', function() {
+      var $wrapper = $(this);
+
+      // Turn the legend into a clickable link, but retain span.field-group-format-toggler
+      // for CSS positioning.
+
+      var $toggler = $('span.field-group-format-toggler:first', $wrapper);
+      var $link = $('<a class="field-group-format-title" href="#"></a>');
+      $link.prepend($toggler.contents());
+
+      // Add required field markers if needed
+      if ($(this).is('.required-fields') && $(this).find('.form-required').length > 0) {
+        $link.append(' ').append($('.form-required').eq(0).clone());
+      }
+
+      $link.appendTo($toggler);
+
+      // .wrapInner() does not retain bound events.
+      $link.click(function () {
+        var wrapper = $wrapper.get(0);
+        // Don't animate multiple times.
+        if (!wrapper.animating) {
+          wrapper.animating = true;
+          var speed = $wrapper.hasClass('speed-fast') ? 300 : 1000;
+          if ($wrapper.hasClass('effect-none') && $wrapper.hasClass('speed-none')) {
+            $('> .field-group-format-wrapper', wrapper).toggle();
+          }
+          else if ($wrapper.hasClass('effect-blind')) {
+            $('> .field-group-format-wrapper', wrapper).toggle('blind', {}, speed);
+          }
+          else {
+            $('> .field-group-format-wrapper', wrapper).toggle(speed);
+          }
+          wrapper.animating = false;
+        }
+        $wrapper.toggleClass('collapsed');
+        return false;
+      });
+
+    });
+  }
+};
+
+/**
+ * Behaviors.
+ */
+Drupal.behaviors.fieldGroup = {
+  attach: function (context, settings) {
+    settings.field_group = settings.field_group || Drupal.settings.field_group;
+    if (settings.field_group == undefined) {
+      return;
+    }
+
+    // Execute all of them.
+    $.each(Drupal.FieldGroup.Effects, function (func) {
+      // We check for a wrapper function in Drupal.field_group as
+      // alternative for dynamic string function calls.
+      var type = func.toLowerCase().replace("process", "");
+      if (settings.field_group[type] != undefined && $.isFunction(this.execute)) {
+        this.execute(context, settings, settings.field_group[type]);
+      }
+    });
+
+    // Fixes css for fieldgroups under vertical tabs.
+    $('.fieldset-wrapper .fieldset > legend').css({display: 'block'});
+    $('.vertical-tabs fieldset.fieldset').addClass('default-fallback');
+
+
+    // Add a new ID to each fieldset.
+    $('.group-wrapper fieldset').each(function() {
+      // Tats bad, but we have to keep the actual id to prevent layouts to break.
+      var fieldgorupID = 'field_group-' + $(this).attr('id') + ' ' + $(this).attr('id');
+      $(this).attr('id', fieldgorupID);
+    })
+    // Set the hash in url to remember last userselection.
+    $('.group-wrapper ul li').each(function() {
+      var fieldGroupNavigationListIndex = $(this).index();
+      $(this).children('a').click(function() {
+        var fieldset = $('.group-wrapper fieldset').get(fieldGroupNavigationListIndex);
+        // Grab the first id, holding the wanted hashurl.
+        var hashUrl = $(fieldset).attr('id').replace(/^field_group-/, '').split(' ')[0];
+        window.location.hash = hashUrl;
+      });
+    });
+  }
+};
+
+})(jQuery);;
+(function($) {
+
+/**
+ * Disable the continue buttons in the checkout process once they are clicked
+ * and provide a notification to the user.
+ */
+Drupal.behaviors.commerceCheckout = {
+  attach: function (context, settings) {
+    // When the buttons to move from page to page in the checkout process are
+    // clicked we disable them so they are not accidently clicked twice.
+    $('input.checkout-continue:not(.checkout-processed)', context).addClass('checkout-processed').click(function() {
+      var $this = $(this);
+      $this.clone().insertAfter(this).attr('disabled', true).next().removeClass('element-invisible');
+      $this.hide();
+    });
+  }
+}
+
+})(jQuery);
+;
+(function($) {
+  Drupal.behaviors.chosen = {
+    attach: function(context, settings) {
+      settings.chosen = settings.chosen || Drupal.settings.chosen;
+
+      // Prepare selector and add unwantend selectors.
+      var selector = settings.chosen.selector;
+
+      // Function to prepare all the options together for the chosen() call.
+      var getElementOptions = function (element) {
+        var options = $.extend({}, settings.chosen.options);
+
+        // The width default option is considered the minimum width, so this
+        // must be evaluated for every option.
+        if ($(element).width() < settings.chosen.minimum_width) {
+          options.width = settings.chosen.minimum_width + 'px';
+        }
+        else {
+          options.width = $(element).width() + 'px';
+        }
+
+        // Some field widgets have cardinality, so we must respect that.
+        // @see chosen_pre_render_select()
+        if ($(element).attr('multiple') && $(element).data('cardinality')) {
+          options.max_selected_options = $(element).data('cardinality');
+        }
+
+        return options;
+      };
+
+      // Process elements that have opted-in for Chosen.
+      // @todo Remove support for the deprecated chosen-widget class.
+      $('select.chosen-enable, select.chosen-widget', context).once('chosen', function() {
+        options = getElementOptions(this);
+        $(this).chosen(options);
+      });
+
+      $(selector, context)
+        // Disabled on:
+        // - Field UI
+        // - WYSIWYG elements
+        // - Tabledrag weights
+        // - Elements that have opted-out of Chosen
+        // - Elements already processed by Chosen
+        .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select, .wysiwyg, .draggable select[name$="[weight]"], .draggable select[name$="[position]"], .chosen-disable, .chosen-processed')
+        .filter(function() {
+          // Filter out select widgets that do not meet the minimum number of
+          // options.
+          var minOptions = $(this).attr('multiple') ? settings.chosen.minimum_multiple : settings.chosen.minimum_single;
+          if (!minOptions) {
+            // Zero value means no minimum.
+            return true;
+          }
+          else {
+            return $(this).find('option').length >= minOptions;
+          }
+        })
+        .once('chosen', function() {
+          options = getElementOptions(this);
+          $(this).chosen(options);
+        });
+    }
+  };
+})(jQuery);
+;
---
+++ sites/default/files/js/js_cJQHaKzNuatQF9H7OR7nBFh99PFjkebAFuE1BaEbGl0.js
@@ -0,0 +1,192 @@
+(function ($) {
+
+/**
+ * A progressbar object. Initialized with the given id. Must be inserted into
+ * the DOM afterwards through progressBar.element.
+ *
+ * method is the function which will perform the HTTP request to get the
+ * progress bar state. Either "GET" or "POST".
+ *
+ * e.g. pb = new progressBar('myProgressBar');
+ *      some_element.appendChild(pb.element);
+ */
+Drupal.progressBar = function (id, updateCallback, method, errorCallback) {
+  var pb = this;
+  this.id = id;
+  this.method = method || 'GET';
+  this.updateCallback = updateCallback;
+  this.errorCallback = errorCallback;
+
+  // The WAI-ARIA setting aria-live="polite" will announce changes after users
+  // have completed their current activity and not interrupt the screen reader.
+  this.element = $('<div class="progress" aria-live="polite"></div>').attr('id', id);
+  this.element.html('<div class="bar"><div class="filled"></div></div>' +
+                    '<div class="percentage"></div>' +
+                    '<div class="message">&nbsp;</div>');
+};
+
+/**
+ * Set the percentage and status message for the progressbar.
+ */
+Drupal.progressBar.prototype.setProgress = function (percentage, message) {
+  if (percentage >= 0 && percentage <= 100) {
+    $('div.filled', this.element).css('width', percentage + '%');
+    $('div.percentage', this.element).html(percentage + '%');
+  }
+  $('div.message', this.element).html(message);
+  if (this.updateCallback) {
+    this.updateCallback(percentage, message, this);
+  }
+};
+
+/**
+ * Start monitoring progress via Ajax.
+ */
+Drupal.progressBar.prototype.startMonitoring = function (uri, delay) {
+  this.delay = delay;
+  this.uri = uri;
+  this.sendPing();
+};
+
+/**
+ * Stop monitoring progress via Ajax.
+ */
+Drupal.progressBar.prototype.stopMonitoring = function () {
+  clearTimeout(this.timer);
+  // This allows monitoring to be stopped from within the callback.
+  this.uri = null;
+};
+
+/**
+ * Request progress data from server.
+ */
+Drupal.progressBar.prototype.sendPing = function () {
+  if (this.timer) {
+    clearTimeout(this.timer);
+  }
+  if (this.uri) {
+    var pb = this;
+    // When doing a post request, you need non-null data. Otherwise a
+    // HTTP 411 or HTTP 406 (with Apache mod_security) error may result.
+    $.ajax({
+      type: this.method,
+      url: this.uri,
+      data: '',
+      dataType: 'json',
+      success: function (progress) {
+        // Display errors.
+        if (progress.status == 0) {
+          pb.displayError(progress.data);
+          return;
+        }
+        // Update display.
+        pb.setProgress(progress.percentage, progress.message);
+        // Schedule next timer.
+        pb.timer = setTimeout(function () { pb.sendPing(); }, pb.delay);
+      },
+      error: function (xmlhttp) {
+        pb.displayError(Drupal.ajaxError(xmlhttp, pb.uri));
+      }
+    });
+  }
+};
+
+/**
+ * Display errors on the page.
+ */
+Drupal.progressBar.prototype.displayError = function (string) {
+  var error = $('<div class="messages error"></div>').html(string);
+  $(this.element).before(error).hide();
+
+  if (this.errorCallback) {
+    this.errorCallback(this);
+  }
+};
+
+})(jQuery);
+;
+(function($) {
+
+/**
+ * Disable the continue buttons in the checkout process once they are clicked
+ * and provide a notification to the user.
+ */
+Drupal.behaviors.commerceCheckout = {
+  attach: function (context, settings) {
+    // When the buttons to move from page to page in the checkout process are
+    // clicked we disable them so they are not accidently clicked twice.
+    $('input.checkout-continue:not(.checkout-processed)', context).addClass('checkout-processed').click(function() {
+      var $this = $(this);
+      $this.clone().insertAfter(this).attr('disabled', true).next().removeClass('element-invisible');
+      $this.hide();
+    });
+  }
+}
+
+})(jQuery);
+;
+(function($) {
+  Drupal.behaviors.chosen = {
+    attach: function(context, settings) {
+      settings.chosen = settings.chosen || Drupal.settings.chosen;
+
+      // Prepare selector and add unwantend selectors.
+      var selector = settings.chosen.selector;
+
+      // Function to prepare all the options together for the chosen() call.
+      var getElementOptions = function (element) {
+        var options = $.extend({}, settings.chosen.options);
+
+        // The width default option is considered the minimum width, so this
+        // must be evaluated for every option.
+        if ($(element).width() < settings.chosen.minimum_width) {
+          options.width = settings.chosen.minimum_width + 'px';
+        }
+        else {
+          options.width = $(element).width() + 'px';
+        }
+
+        // Some field widgets have cardinality, so we must respect that.
+        // @see chosen_pre_render_select()
+        if ($(element).attr('multiple') && $(element).data('cardinality')) {
+          options.max_selected_options = $(element).data('cardinality');
+        }
+
+        return options;
+      };
+
+      // Process elements that have opted-in for Chosen.
+      // @todo Remove support for the deprecated chosen-widget class.
+      $('select.chosen-enable, select.chosen-widget', context).once('chosen', function() {
+        options = getElementOptions(this);
+        $(this).chosen(options);
+      });
+
+      $(selector, context)
+        // Disabled on:
+        // - Field UI
+        // - WYSIWYG elements
+        // - Tabledrag weights
+        // - Elements that have opted-out of Chosen
+        // - Elements already processed by Chosen
+        .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select, .wysiwyg, .draggable select[name$="[weight]"], .draggable select[name$="[position]"], .chosen-disable, .chosen-processed')
+        .filter(function() {
+          // Filter out select widgets that do not meet the minimum number of
+          // options.
+          var minOptions = $(this).attr('multiple') ? settings.chosen.minimum_multiple : settings.chosen.minimum_single;
+          if (!minOptions) {
+            // Zero value means no minimum.
+            return true;
+          }
+          else {
+            return $(this).find('option').length >= minOptions;
+          }
+        })
+        .once('chosen', function() {
+          options = getElementOptions(this);
+          $(this).chosen(options);
+        });
+    }
+  };
+})(jQuery);
+;
